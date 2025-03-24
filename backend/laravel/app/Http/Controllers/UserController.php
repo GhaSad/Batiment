a2548e522cb890509c512a5da0b8200442bfc19b
nahel95 <nahel.sait95@gmail.com>
2025-03-24 14:29:58 +0100

    Revert "Renommage de admin.html en home.html et résolution du conflit"
    This reverts commit f0af5556da9578c9bb9eb5b4f6c0cb4849f58d3f, reversing
    changes made to add3c81ea103cb5fa63b08db28fc7856931c06a7.

--- backend/laravel/app/Http/Controllers/UserController.php
+++ backend/laravel/app/Http/Controllers/UserController.php
@@ -3,52 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    // Afficher le formulaire d'inscription
-    public function showRegistrationForm()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return view('register');  // Vue de formulaire d'inscription
+        return response()->json(User::all());
     }
 
-    // Gérer l'inscription
-    public function register(Request $request)
-{
-    // Validation des données
-    $validated = $request->validate([
-        'prenom' => 'required|string|max:255',
-        'nom' => 'required|string|max:255',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|min:6|confirmed',
-    ]);
-
-    // Création de l'utilisateur
-    try {
-        $user = User::create([
-            'username' => $request->prenom . ' ' . $request->nom,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'role' => 'visiteur',
-        ]);
-
-        Log::info('Utilisateur créé : ', ['user' => $user]);
-
-    } catch (\Exception $e) {
-        // Si une erreur se produit pendant la création de l'utilisateur, afficher l'exception
-        return back()->with('error', 'Une erreur est survenue : ' . $e->getMessage());
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    // Vérifie si l'utilisateur a été créé avec succès
-    if ($user) {
-        return redirect()->route('register.form')->with('success', 'Inscription réussie !');
-    } else {
-        return redirect()->route('register.form')->with('error', 'Une erreur est survenue.');
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
     }
-}
 
-    
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
